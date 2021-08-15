c8c37d1d0f5a9243562648bf75ed91f1bbd719f0
rgoshen <rick.goshen@gmail.com>
2021-08-15 16:51:36 -0700

    refactor cardlist component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,5 +2,11 @@ import React from "react";
 import "./card-list.styles.css";
 
 export const CardList = (props) => {
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <h2 key={monster.id}>{monster.name}</h2>
+      ))}
+    </div>
+  );
 };
