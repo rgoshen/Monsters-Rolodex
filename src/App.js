64861b4e778d27958e794d3c7e4b307618f304ca
rgoshen <rick.goshen@gmail.com>
2021-08-16 19:07:37 -0700

    refactor onHandleChange to onSearchChange

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -28,7 +32,7 @@ class App extends Component {
         <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          onSearchChange={this.onSearchChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
